020aec965fb4f67ff8c64be6d9574eec0f85acaa
hiepphatle1104 <phatlee1104@gmail.com>
2025-10-16 00:02:17 +0700

    init customer-service module

---
+++ customer-service/src/main/java/org/trading/platform/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.trading.platform.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+}
