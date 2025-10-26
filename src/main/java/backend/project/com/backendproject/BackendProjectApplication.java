ba1a674b68e9bb68aafda8158aba0711da068e26
Quốc Lâm <lamquoc07122004@gmail.com>
2025-10-26 20:21:18 +0700

    new project

---
+++ src/main/java/backend/project/com/backendproject/BackendProjectApplication.java
@@ -0,0 +1,13 @@
+package backend.project.com.backendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendProjectApplication.class, args);
+    }
+
+}
