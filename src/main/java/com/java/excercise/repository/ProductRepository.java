770cfacc030922b793346dc3fc441a3a0e3c69ff
Quốc Lâm <lamquoc07122004@gmail.com>
2025-11-08 12:07:48 +0700

    Panigation and Search

--- src/main/java/com/java/excercise/repository/ProductRepository.java
+++ src/main/java/com/java/excercise/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.java.excercise.repository;
 
 import com.java.excercise.model.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,7 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
     List<Product> findAllByUserId(String userId);
+
+    Page<Product> findAll(Pageable pageable);
+    Page<Product> findByNameContainingIgnoreCase(String nameKey, Pageable pageable);
 }
